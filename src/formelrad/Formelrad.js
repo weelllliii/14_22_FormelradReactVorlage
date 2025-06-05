7f6c2d65421cb9ad8644498c50a7ee0492e3e40d
Wellington <wellidias2000@gmail.com>
2025-06-05 09:03:05 +0200

    0004-For-the-first-demo-a-basic-form-has-to-be-shown

--- src/formelrad/Formelrad.js
+++ src/formelrad/Formelrad.js
@@ -1,6 +1,22 @@
+import formelrad from "../image/formelradelektronik.gif";
+
 export default function Formelrad() {
     return (
         <>
+            <h2>Formelrad</h2>
+            <img src={formelrad} width="200" alt="Formelrad"/>
+            <form>
+                <div>
+                    <label>Spannung</label><input></input>
+                </div>
+                <div>
+                    <label>Strom</label><input></input>
+                </div>
+                <div>
+                    <label>Widerstand</label><input></input>
+                </div>
+                <button type="submit">Calculate</button>
+            </form>
         </>
     )
 }
