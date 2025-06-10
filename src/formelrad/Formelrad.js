7ba68be6c1c07a66cb9c529fe91f814cb2c4ec6e
Wellington <wellidias2000@gmail.com>
2025-06-10 14:35:38 +0200

    0011-Formelrad-component-has-to-use-mvp.styling

--- src/formelrad/Formelrad.js
+++ src/formelrad/Formelrad.js
@@ -1,22 +1,27 @@
+import '../css/mvp.css';
 import formelrad from "../image/formelradelektronik.gif";
 
 export default function Formelrad() {
     return (
         <>
-            <h2>Formelrad</h2>
-            <img src={formelrad} width="200" alt="Formelrad"/>
-            <form>
-                <div>
-                    <label>Spannung</label><input></input>
-                </div>
-                <div>
-                    <label>Strom</label><input></input>
-                </div>
-                <div>
-                    <label>Widerstand</label><input></input>
-                </div>
-                <button type="submit">Calculate</button>
-            </form>
+            <section>
+                <header>
+                    <h2>Formelrad</h2>
+                    <img src={formelrad} width="200" alt="Formelrad"/>
+                </header>
+                <form>
+                    <div>
+                        <label>Spannung</label><input></input>
+                    </div>
+                    <div>
+                        <label>Strom</label><input></input>
+                    </div>
+                    <div>
+                        <label>Widerstand</label><input></input>
+                    </div>
+                    <button type="submit">Calculate</button>
+                </form>
+            </section>
         </>
     )
 }
