096bcfb81162fff9c213fb0b2d290e2d5ee26ebd
Wellington <wellidias2000@gmail.com>
2025-06-11 08:57:30 +0200

    0012-Power-is-also-required

--- src/formelrad/Formelrad.js
+++ src/formelrad/Formelrad.js
@@ -7,7 +7,8 @@ export default function Formelrad() {
     const [values, setValues] = useState({
         u: 10,
         i: 2,
-        r: ""
+        r: "",
+        p: ""
     })
     return (
         <>
@@ -20,6 +21,7 @@ export default function Formelrad() {
                     <InputField color={"black"} value={values.u} label="Spannung" handleChange={e => {setValues(values => ({...values, u: e.target.value}))}} />
                     <InputField color={"black"} value={values.i} label="Stromstärke" handleChange={e => {setValues(values => ({...values, i: e.target.value}))}} />
                     <InputField color={"black"} value={values.r} label="Widerstand" handleChange={e => {setValues(values => ({...values, r: e.target.value}))}} />
+                    <InputField color={"black"} value={values.p} label="Leistung" handleChange={e => {setValues(values => ({...values, p: e.target.value}))}} />
                     <button type="submit">Calculate</button>
                 </form>
             </section>
