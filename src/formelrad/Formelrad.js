193f95839a4048c2fac9e62a78c5b3a63032bccc
Wellington <wellidias2000@gmail.com>
2025-06-11 08:53:33 +0200

    Merge conflicts fixed

--- src/formelrad/Formelrad.js
+++ src/formelrad/Formelrad.js
@@ -9,32 +9,17 @@ export default function Formelrad() {
         i: 2,
         r: ""
     })
-
     return (
         <>
-            <h2>Formelrad</h2>
-            <img src={formelrad} width="200" alt="Formelrad"/>
-            <form>
-                <InputField color={"black"} value={values.u} label="Spannung" handleChange={e => {setValues(values => ({...values, u: e.target.value}))}} />
-                <InputField color={"black"} value={values.i} label="Stromstaerke" handleChange={e => {setValues(values => ({...values, i: e.target.value}))}} />
-                <InputField color={"black"} value={values.r} label="Widerstand" handleChange={e => {setValues(values => ({...values, r: e.target.value}))}} />
-                <button type="submit">Calculate</button>
-            </form>
             <section>
                 <header>
                     <h2>Formelrad</h2>
                     <img src={formelrad} width="200" alt="Formelrad"/>
                 </header>
                 <form>
-                    <div>
-                        <label>Spannung</label><input></input>
-                    </div>
-                    <div>
-                        <label>Strom</label><input></input>
-                    </div>
-                    <div>
-                        <label>Widerstand</label><input></input>
-                    </div>
+                    <InputField color={"black"} value={values.u} label="Spannung" handleChange={e => {setValues(values => ({...values, u: e.target.value}))}} />
+                    <InputField color={"black"} value={values.i} label="Stromstärke" handleChange={e => {setValues(values => ({...values, i: e.target.value}))}} />
+                    <InputField color={"black"} value={values.r} label="Widerstand" handleChange={e => {setValues(values => ({...values, r: e.target.value}))}} />
                     <button type="submit">Calculate</button>
                 </form>
             </section>
