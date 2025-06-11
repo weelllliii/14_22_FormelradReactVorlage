28478e0e4cc9e40f67169014a3551836af037e5e
Wellington <wellidias2000@gmail.com>
2025-06-11 09:04:06 +0200

    0017-Calculate-i-and-p

--- src/formelrad/Formelrad.js
+++ src/formelrad/Formelrad.js
@@ -10,6 +10,22 @@ export default function Formelrad() {
         r: "",
         p: ""
     })
+
+    const calculate = (event) => {
+        event.preventDefault();
+        console.log("calculate");
+        if (values.i === "" && values.r === "") {
+            /*calculate i and r */
+            setValues(values => ({...values, i: values.p / values.u}));
+            setValues(values => ({...values, r: values.u * values.u / values.p}));
+        } else if (values.i === "" && values.p === "") {
+            /*calculate i and p */
+            setValues(values => ({...values, i: values.u / values.r}));
+            setValues(values => ({...values, p: values.u * values.u / values.r}));
+            setColors( colors=> ({...colors, i:"red", p:"red"}));
+        }
+    }
+
     return (
         <>
             <section>
@@ -17,7 +33,7 @@ export default function Formelrad() {
                     <h2>Formelrad</h2>
                     <img src={formelrad} width="200" alt="Formelrad"/>
                 </header>
-                <form>
+                <form onSubmit={calculate}>
                     <InputField color={"black"} value={values.u} label="Spannung" handleChange={e => {setValues(values => ({...values, u: e.target.value}))}} />
                     <InputField color={"black"} value={values.i} label="Stromstärke" handleChange={e => {setValues(values => ({...values, i: e.target.value}))}} />
                     <InputField color={"black"} value={values.r} label="Widerstand" handleChange={e => {setValues(values => ({...values, r: e.target.value}))}} />
