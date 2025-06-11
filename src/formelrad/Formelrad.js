2b94edf946ef32b2fee82e2f1ed49e4691e1bcf8
Wellington <wellidias2000@gmail.com>
2025-06-11 09:26:39 +0200

    0023-Use-color-from-data-for-the-fields

--- src/formelrad/Formelrad.js
+++ src/formelrad/Formelrad.js
@@ -11,6 +11,14 @@ export default function Formelrad() {
         p: ""
     })
 
+    const [colors, setColors] = useState({
+        u: "black",
+        i: "black",
+        r: "black",
+        p: "black",
+        message: "red"
+    })
+
     const handleSubmit = (event) => {
         event.preventDefault();
         console.log("handleSubmit")
@@ -49,10 +57,10 @@ export default function Formelrad() {
                     <img src={formelrad} width="200" alt="Formelrad"/>
                 </header>
                 <form onSubmit={handleSubmit}>
-                    <InputField color={"black"} value={values.u} label="Spannung" handleChange={e => {setValues(values => ({...values, u: e.target.value}))}} />
-                    <InputField color={"black"} value={values.i} label="Stromstärke" handleChange={e => {setValues(values => ({...values, i: e.target.value}))}} />
-                    <InputField color={"black"} value={values.r} label="Widerstand" handleChange={e => {setValues(values => ({...values, r: e.target.value}))}} />
-                    <InputField color={"black"} value={values.p} label="Leistung" handleChange={e => {setValues(values => ({...values, p: e.target.value}))}} />
+                    <InputField color={colors.u} value={values.u} label="Spannung" handleChange={e => {setValues(values => ({...values, u: e.target.value}))}} />
+                    <InputField color={colors.i} value={values.i} label="Stromstärke" handleChange={e => {setValues(values => ({...values, i: e.target.value}))}} />
+                    <InputField color={colors.r} value={values.r} label="Widerstand" handleChange={e => {setValues(values => ({...values, r: e.target.value}))}} />
+                    <InputField color={colors.p} value={values.p} label="Leistung" handleChange={e => {setValues(values => ({...values, p: e.target.value}))}} />
                     <button type="submit">Calculate</button>
                 </form>
             </section>
