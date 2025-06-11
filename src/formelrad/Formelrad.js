72121ca51f70c24e69e4c387d5443cbbbbde6a0f
Wellington <wellidias2000@gmail.com>
2025-06-11 09:27:08 +0200

    0024-If-field-is-calculated-text-shall-be-red

--- src/formelrad/Formelrad.js
+++ src/formelrad/Formelrad.js
@@ -26,26 +26,32 @@ export default function Formelrad() {
             /*calculate u and i */
             setValues(values => ({...values, u: Math.sqrt(values.p * values.r)}));
             setValues(values => ({...values, i: Math.sqrt(values.p / values.r)}));
+            setColors( colors=> ({...colors, u:"red", i:"red"}));
         } else if (values.u === "" && values.r === "") {
             /*calculate u and r */
             setValues(values => ({...values, u: values.p / values.i}));
             setValues(values => ({...values, r: values.p / values.i / values.i}));
+            setColors( colors=> ({...colors, u:"red", r:"red"}));
         } else if (values.u === "" && values.p === "") {
             /*calculate u and p */
             setValues(values => ({...values, u: values.i * values.r}));
             setValues(values => ({...values, p: values.i * values.i * values.r}));
+            setColors( colors=> ({...colors, u:"red", p:"red"}));
         } else if (values.i === "" && values.r === "") {
             /*calculate i and r */
             setValues(values => ({...values, i: values.p / values.u}));
             setValues(values => ({...values, r: values.u * values.u / values.p}));
+            setColors( colors=> ({...colors, i:"red", r:"red"}));
         } else if (values.i === "" && values.p === "") {
             /*calculate i and p */
             setValues(values => ({...values, i: values.u / values.r}));
             setValues(values => ({...values, p: values.u * values.u / values.r}));
+            setColors( colors=> ({...colors, i:"red", p:"red"}));
         } else {
             /*calculate r and p */
             setValues(values => ({...values, r: values.u / values.i}));
             setValues(values => ({...values, p: values.u * values.i}));
+            setColors( colors=> ({...colors, r:"red", p:"red"}));
         }
     }
 
